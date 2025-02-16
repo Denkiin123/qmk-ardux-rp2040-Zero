c9e858e14721bbde8611b79ad37c14cd0be304f6
Denkiin123 <denkiinart.34@gmail.com>
2025-02-17 00:20:32 +0800

    Update rules.mk
    New Pins

--- keyboards/ardux/thepaintbrush/rules.mk
+++ keyboards/ardux/thepaintbrush/rules.mk
@@ -13,10 +13,10 @@ LTO_ENABLE = no # We support arm qmk devices which are incompatabl with this avr
 
 # Work around limitation with userland and the way we have 'dynamic' direct wiring
 #     This *should* live in config.h but KemoNine can't figure out how to check which keymap is in use at that level
-PINS_HAND_LEFT = -DDIRECT_PINS="{ { F7, F6, F5, F4 }, { B6, B5, B4, E6 } }"
+PINS_HAND_LEFT = -DDIRECT_PINS="{ { F7, F6, F5, F4 }, { D7, B6, B5, B4 } }"
 #     Pins 26, 27,28, 29 and 10, 9, 8, 7 - Hi
-
-PINS_HAND_RIGHT = -DDIRECT_PINS="{ { F4, F5, F6, F7 }, { B1, B3, B2, B6 } }"
+#     OLD - F7, F6, F5, F4 - B6, B5, B4, E6
+PINS_HAND_RIGHT = -DDIRECT_PINS="{ { F4, F5, F6, F7 }, { B4, B5, B6, D7 } }"
 ifeq ($(KEYBOARD), ardux/thepaintbrush)
 	ifeq ($(KEYMAP), left)
 		OPT_DEFS += $(PINS_HAND_LEFT)
